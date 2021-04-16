913cd949bd2fe818a1f18505d85634cbb599e5b6
Thiago Piffer Ramos <thiago.ramos@betha.com.br>
2021-04-16 11:12:06 -0300

    Criação de duas novas diretivas

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -1,22 +1,16 @@
 angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope, contatosAPI, operadorasAPI, serialGenerator){
     $scope.app = "Lista Telefonica";
-    $scope.contatos = [
-        // {nome: "Pedro", telefone: "9999-8888", date: new Date(), operadora: {nome: "Oi", codigo: 14, categoria: "Celular"}, color: "red"},
-        // {nome: "Ana", telefone: "9523-4458", date: new Date(), operadora: {nome: "Vivo", codigo: 15, categoria: "Celular"}, color: "red"},
-        // {nome: "Maria", telefone: "4524-4455", date: new Date(), operadora: {nome: "Tim", codigo: 41, categoria: "Celular"}, color: "red"}
-    ];
-    $scope.operadoras = [
-        // {nome: "Oi", codigo: 14, categoria: "Celular", preco: 1},
-        // {nome: "Vivo", codigo: 15, categoria: "Celular", preco: 2},
-        // {nome: "Tim", codigo: 41, categoria: "Celular", preco: 3},
-        // {nome: "GVT", codigo: 25, categoria: "Fixo", preco: 4},
-        // {nome: "Embratel", codigo: 21, categoria: "Fixo", preco: 5}
-    ];
+    $scope.contatos = [];
+    $scope.operadoras = [];
+    $scope.contato = {
+        data: new Date()
+    };
+
     var carregarContatos = function(){
         contatosAPI.getContatos().then(function(data) {
             $scope.contatos = data.data;
         }).catch(function(data){
-            $scope.message = "Algo de errado não está certo: " + data;
+            $scope.error = "Não foi possível carregar os dados: " + data.status;
         });
     };
     var carregarOperadoras = function() {
