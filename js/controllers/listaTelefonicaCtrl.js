6956e0649a04f1e66d3ab9dada4f4b7cf001d00f
Thiago Piffer Ramos <thiago.ramos@betha.com.br>
2021-04-19 11:47:47 -0300

    Route Configs

--- js/controllers/listaTelefonicaCtrl.js
+++ js/controllers/listaTelefonicaCtrl.js
@@ -1,23 +1,11 @@
-angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope, contatosAPI, operadorasAPI, serialGenerator){
+angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($scope, contatos, contatosAPI, operadoras, serialGenerator){
     $scope.app = "Lista Telefonica";
-    $scope.contatos = [];
-    $scope.operadoras = [];
+    $scope.contatos = contatos.data;
+    $scope.operadoras = operadoras.data;
     $scope.contato = {
         data: new Date()
     };
 
-    var carregarContatos = function(){
-        contatosAPI.getContatos().then(function(data) {
-            $scope.contatos = data.data;
-        }).catch(function(data){
-            $scope.error = "Não foi possível carregar os dados: " + data.status;
-        });
-    };
-    var carregarOperadoras = function() {
-        operadorasAPI.getOperadoras().then(function(data) {
-            $scope.operadoras = data.data;
-        });
-    };
     $scope.adicionarContato = function(contato){
         contato.serial = serialGenerator.generate();
         contato.data = new Date();
@@ -33,7 +21,6 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
                 contatosAPI.deleteContato(contato).then(function(response){
                     console.log(response.data);
                 });
-                console.log(contato.id);
             };
         });
     }
@@ -47,6 +34,4 @@ angular.module("listaTelefonica").controller("listaTelefonicaCtrl", function($sc
         $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
     }
 
-    carregarContatos();
-    carregarOperadoras();
 });
